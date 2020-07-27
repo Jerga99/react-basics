26b9af1fc1cad91cc5acbdacb51a77349f5bd05c
Filip Jerga <jerga99@gmail.com>
2020-07-28 00:00:28 +0200

    Class Component

--- src/App.js
+++ src/App.js
@@ -9,13 +9,25 @@ import React from 'react';
 //   )
 // }
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1>Hello World</h1>
-      <h2>Hey Guys</h2>
-    </div>
-  )
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <h1>Hello World</h1>
+//       <h2>Hey Guys</h2>
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World from Class Component</h1>
+        <h2>Hey Guys!</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
