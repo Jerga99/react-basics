d636889a23a1481407455fd9ee445815e4ed2bf4
Filip Jerga <jerga99@gmail.com>
2020-07-28 00:05:44 +0200

    State debugging in class component

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ import React from 'react';
 
 class App extends React.Component {
 
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     counter: 0
+  //   }
+  // }
+
   state = {
     counter: 0
   }
