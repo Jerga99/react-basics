d1f3245f55d2b45950a237ff5ae8f34487813a6c
Filip Jerga <jerga99@gmail.com>
2020-07-29 10:30:52 +0200

    Moving layout to components

---
+++ src/components/ResourceSearch.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const ResourceSearch = () => {
+
+  return (
+    <form className="card p-2">
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Some title" />
+        <div className="input-group-append">
+          <button type="submit" className="btn btn-secondary">Search</button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default ResourceSearch;
