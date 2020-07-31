a3f5de53a19f38d3047973493cb8fef024c71e71
Filip Jerga <jerga99@gmail.com>
2020-07-31 13:50:42 +0200

    Initial resource list

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Header = () =>
   <div className="py-5 text-center">
-    <img style={{height: '150px'}} src={process.env.PUBLIC_URL + '/logo512.png'} />
+    <img
+      alt="React logo"
+      style={{height: '150px'}}
+      src={process.env.PUBLIC_URL + '/logo512.png'} />
     <h2>Keep Resources</h2>
     <p className="lead">Keep your resource at once place</p>
   </div>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,52 +1,49 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialResources = [
+  {
+    _id: '1',
+    title: 'Resource 1',
+    description: 'Description 1',
+    link: 'https://google.com',
+    type: 'blog'
+  },
+  {
+    _id: '2',
+    title: 'Resource 2',
+    description: 'Description 2',
+    link: 'https://google.com',
+    type: 'video'
+  },
+  {
+    _id: '3',
+    title: 'Resource 3',
+    description: 'Description 3',
+    link: 'https://google.com',
+    type: 'book'
+  }
+]
+
+// 1. create state with initialResources
+// 2. Iterate resources
+// 3. display resouce info
+
 
 const ResourceList = () => {
+  const [resources, setResources] = useState(initialResources);
 
   return (
     <ul className="list-group mb-3 resource-list">
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Resource 1</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Resource 2</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between lh-condensed">
-        <div>
-          <h6 className="my-0">Resource 3</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between">
-        <div>
-          <h6 className="my-0">Resource 4</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between">
-        <div>
-          <h6 className="my-0">Resource 5</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
-      <li className="list-group-item d-flex justify-content-between bg-light">
-        <div className="text-success">
-          <h6 className="my-0">Resource 6</h6>
-          <small className="text-muted">Brief description</small>
-        </div>
-        <span className="text-muted">type</span>
-      </li>
+      { resources.map(resource =>
+        <li key={resource._id} className="list-group-item d-flex justify-content-between lh-condensed">
+          <div>
+            <h6 className="my-0">{resource.title}</h6>
+            <small className="text-muted">{resource.description}</small>
+          </div>
+          <span className="text-muted">{resource.type}</span>
+        </li>
+      )}
     </ul>
   )
 }
