9943687a79fecbbdc88db9e0bc8002f65a4a475b
Filip Jerga <jerga99@gmail.com>
2020-08-03 09:58:49 +0200

    Reset alert

--- src/components/ResourceUpdate.js
+++ src/components/ResourceUpdate.js
@@ -1,17 +1,27 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ResourceForm from './ResourceForm';
 import { updateResourceApi } from '../actions';
 
-const createAlert = () => ({success: null, error: null})
+const initAlert = () => ({success: null, error: null})
 
 const ResourceUpdate = ({resource, onResourceUpdate}) => {
-  const [alert, setAlert] = useState(createAlert());
+  const [alert, setAlert] = useState(initAlert());
+
+  const resetAlert = () => setAlert(initAlert());
+
+  useEffect(() => {
+    resetAlert();
+  }, [resource]);
 
   const displayAlert = (type, message) => {
-    const _alert = createAlert();
+    const _alert = initAlert();
     _alert[type] = message;
     setAlert(_alert);
+    setTimeout(() => {
+      window.alert('Hello World');
+      resetAlert();
+    }, 3000);
   }
 
   const updateResource = async (resourceData) => {
