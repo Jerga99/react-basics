2198082b471051b846568d95a9685e20328bdbd1
Filip Jerga <jerga99@gmail.com>
2020-08-04 10:12:29 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@
 
 import React from 'react';
 import ResourceHome from './pages/ResourceHome';
+import Header from './components/Header';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <ResourceHome />
+    <div className="container">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <ResourceHome />
+          </Route>
+          <Route path="/new">
+            <h1>Resource new page</h1>
+          </Route>
+          <Route path="/detail">
+            <h1>Resource detail page</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
--- src/pages/ResourceHome.js
+++ src/pages/ResourceHome.js
@@ -4,7 +4,6 @@ import ResourceSearch from '../components/ResourceSearch';
 import ResourceList from '../components/ResourceList';
 import ResourceDetail from '../components/ResourceDetail';
 import ResourceUpdate from '../components/ResourceUpdate';
-import Header from '../components/Header';
 
 import { getResources, deleteResourceApi } from '../actions';
 
@@ -68,44 +67,41 @@ const ResourceHome = () => {
   const activeResource = selectedResource || (hasResources && resources[0]) || null;
 
   return (
-    <div className="container">
-      <Header />
-      <div className="row">
-        <div className="col-md-4 order-md-2 mb-4">
-          <h4 className="d-flex justify-content-between align-items-center mb-3">
-            <span className="text-muted">Your Resources</span>
-            <span className="badge badge-secondary badge-pill">{resources.length}</span>
-          </h4>
-          <ResourceSearch />
-          <ResourceList
-            activeId={activeResource?._id}
-            onItemClick={setSetlectedResource}
-            resources={resources} />
-        </div>
-        <div className="col-md-8 order-md-1">
-          <h4 className="mb-3">Resource {activeResource?._id}
-            { hasResources &&
-              <>
-                <button
-                  onClick={() => setDetailView(!isDetailView)}
-                  className={`btn btn-sm ml-2 mr-2 ${isDetailView ? 'btn-warning' : 'btn-primary'}`}>
-                  { isDetailView ? 'Edit' : 'Detail'}
-                </button>
-                <button
-                  onClick={deleteResource}
-                  className="btn btn-danger btn-sm">
-                  Delete
-                </button>
-              </>
-            }
-          </h4>
-          { isDetailView ?
-            <ResourceDetail resource={activeResource} /> :
-            <ResourceUpdate
-              resource={activeResource}
-              onResourceUpdate={handleResourceUpdate}
-            /> }
-        </div>
+    <div className="row">
+      <div className="col-md-4 order-md-2 mb-4">
+        <h4 className="d-flex justify-content-between align-items-center mb-3">
+          <span className="text-muted">Your Resources</span>
+          <span className="badge badge-secondary badge-pill">{resources.length}</span>
+        </h4>
+        <ResourceSearch />
+        <ResourceList
+          activeId={activeResource?._id}
+          onItemClick={setSetlectedResource}
+          resources={resources} />
+      </div>
+      <div className="col-md-8 order-md-1">
+        <h4 className="mb-3">Resource {activeResource?._id}
+          { hasResources &&
+            <>
+              <button
+                onClick={() => setDetailView(!isDetailView)}
+                className={`btn btn-sm ml-2 mr-2 ${isDetailView ? 'btn-warning' : 'btn-primary'}`}>
+                { isDetailView ? 'Edit' : 'Detail'}
+              </button>
+              <button
+                onClick={deleteResource}
+                className="btn btn-danger btn-sm">
+                Delete
+              </button>
+            </>
+          }
+        </h4>
+        { isDetailView ?
+          <ResourceDetail resource={activeResource} /> :
+          <ResourceUpdate
+            resource={activeResource}
+            onResourceUpdate={handleResourceUpdate}
+          /> }
       </div>
     </div>
   )
