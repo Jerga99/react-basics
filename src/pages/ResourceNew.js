90caf52f5d626e04de4f12b5b671596d46d7b469
Filip Jerga <jerga99@gmail.com>
2020-08-04 11:13:17 +0200

    Create resource, more use cases

--- src/pages/ResourceNew.js
+++ src/pages/ResourceNew.js
@@ -2,24 +2,29 @@
 
 import React from 'react';
 import ResourceForm from '../components/ResourceForm';
+import { useHistory } from 'react-router-dom';
+import withAlert from '../hoc/withAlert';
 
 import { createResourceApi } from '../actions';
 
-const ResourceNew = () => {
+const ResourceNew = ({alert, displayAlert}) => {
+  const router = useHistory();
 
   const createResource = async resource => {
     try {
-      await createResourceApi(resource);
+      const { _id } = await createResourceApi(resource);
+      router.push(`/resources/${_id}`);
     } catch(e) {
-
+      displayAlert('error', e);
     }
   }
 
   return (
     <ResourceForm
       onSubmit={createResource}
+      alert={alert}
     />
   )
 }
 
-export default ResourceNew;
+export default withAlert(ResourceNew);
